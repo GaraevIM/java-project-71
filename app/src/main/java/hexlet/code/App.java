003b98afcdb9c85c3b7beff4619ab2b7a18c3fb0
Ильдар <https://github.com/araev/java-project-61>
2025-11-04 18:51:49 +0700

    Реализован вычислитель отличий и добавлена демонстрация работы (asciinema)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,14 +5,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
 import java.util.concurrent.Callable;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Command(
         name = "gendiff",
@@ -39,31 +32,13 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
-        Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
-
-        String content1 = Files.readString(path1);
-        String content2 = Files.readString(path2);
-
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> data1 = mapper.readValue(content1, new TypeReference<Map<String, Object>>() {});
-        Map<String, Object> data2 = mapper.readValue(content2, new TypeReference<Map<String, Object>>() {});
-
-        System.out.println("Files parsed successfully:");
-        System.out.println("  " + path1);
-        System.out.println("  " + path2);
-        System.out.println("Chosen format: " + format);
-
+        String diff = Differ.generate(filepath1, filepath2);
+        System.out.println(diff);
         return 0;
     }
 
     public static void main(String[] args) {
-        try {
-            int exit = new CommandLine(new App()).execute(args);
-            System.exit(exit);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
+        int exit = new CommandLine(new App()).execute(args);
+        System.exit(exit);
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,68 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeSet;
+
+public final class Differ {
+
+    private Differ() { }
+
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        Path p1 = Paths.get(filePath1).toAbsolutePath().normalize();
+        Path p2 = Paths.get(filePath2).toAbsolutePath().normalize();
+
+        String c1 = Files.readString(p1);
+        String c2 = Files.readString(p2);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> m1 = mapper.readValue(c1, new TypeReference<Map<String, Object>>() { });
+        Map<String, Object> m2 = mapper.readValue(c2, new TypeReference<Map<String, Object>>() { });
+
+        var keys = new TreeSet<String>();
+        keys.addAll(m1.keySet());
+        keys.addAll(m2.keySet());
+
+        var sb = new StringBuilder();
+        sb.append("{\n");
+
+        for (String key : keys) {
+            boolean in1 = m1.containsKey(key);
+            boolean in2 = m2.containsKey(key);
+            Object v1 = m1.get(key);
+            Object v2 = m2.get(key);
+
+            if (in1 && in2) {
+                if (Objects.equals(v1, v2)) {
+                    sb.append("    ").append(key).append(": ").append(stringify(v1)).append("\n");
+                } else {
+                    sb.append("  - ").append(key).append(": ").append(stringify(v1)).append("\n");
+                    sb.append("  + ").append(key).append(": ").append(stringify(v2)).append("\n");
+                }
+            } else if (in1) {
+                sb.append("  - ").append(key).append(": ").append(stringify(v1)).append("\n");
+            } else {
+                sb.append("  + ").append(key).append(": ").append(stringify(v2)).append("\n");
+            }
+        }
+
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private static String stringify(Object value) {
+        if (value == null) {
+            return "null";
+        }
+        if (value instanceof String s) {
+            return s;
+        }
+        return String.valueOf(value);
+    }
+}
