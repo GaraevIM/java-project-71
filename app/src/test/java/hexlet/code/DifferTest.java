79a7c27a7de57056c72d3281eac8d9e5bed09bc4
Ильдар <https://github.com/araev/java-project-61>
2026-01-01 22:03:21 +0400

    fix checkstyle import

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,11 +1,11 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.Differ;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
-import hexlet.Differ;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
